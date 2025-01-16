7e82cbfd85ad540709c5fc61c8291000a28cc158
Venkat Palagiri <44697660+venkateshpalagiri@users.noreply.github.com>
2025-01-15 23:39:35 -0500

    need to work on it

---
+++ src/main/java/com/venkatesh/ProductService/model/ProductResponse.java
@@ -0,0 +1,47 @@
+package com.venkatesh.ProductService.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Column;
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ProductResponse {
+
+    private Long productId;
+    private String productName;
+    private long productPrice;
+    private long productQuantity;
+
+
+//    public ProductResponse() {
+//
+//    }
+//    public ProductResponse(Long productId, String productName, long productPrice, long productQuantity) {
+//        this.productId = productId;
+//        this.productName = productName;
+//        this.productPrice = productPrice;
+//        this.productQuantity = productQuantity;
+//    }
+
+
+
+    public void setProductId(Long productId) {
+        this.productId = productId;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    public void setProductPrice(long productPrice) {
+        this.productPrice = productPrice;
+    }
+
+    public void setProductQuantity(long productQuantity) {
+        this.productQuantity = productQuantity;
+    }
+
+}
